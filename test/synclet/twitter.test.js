aeb45d29cf22062f82d5129f138091b686742c36
Forrest L Norvell <ogd@aoaioxxysz.net>
2012-03-22 21:28:43 -0700

    Add test for Twitter tweets.
    Adding the test for Twitter tweets also required adding some defensive
    coding to the tweets synclet, because it did not cope well with
    missing configuration parameters.
    
    Nuked tests/synclets-local-twitter.js, as it has now been migrated
    entirely from vows --> mocha. 1 down, kabajillion to go!

--- test/synclet/twitter.test.js
+++ test/synclet/twitter.test.js
@@ -6,6 +6,7 @@ var mocha    = require('mocha')
   , friends  = require(path.join(__dirname, '..', '..', 'Connectors', 'Twitter', 'friends.js'))
   , timeline = require(path.join(__dirname, '..', '..', 'Connectors', 'Twitter', 'timeline.js'))
   , mentions = require(path.join(__dirname, '..', '..', 'Connectors', 'Twitter', 'mentions.js'))
+  , tweets   = require(path.join(__dirname, '..', '..', 'Connectors', 'Twitter', 'tweets.js'))
   , util     = require('util')
   ;
 
@@ -84,6 +85,25 @@ describe("twitter connector", function () {
         if (err) return done(err);
 
         response.data.mention[0].id_str.should.equal('71348168469643264');
+        return done();
+      });
+    });
+  });
+
+  describe("tweets synclet", function() {
+    before(function (done) {
+      fakeweb.registerUri({uri : apiBase + 'account/verify_credentials.json?path=%2Faccount%2Fverify_credentials.json' + apiSuffix,
+                           file : __dirname + '/../fixtures/synclets/twitter/verify_credentials.js'});
+      fakeweb.registerUri({uri : apiBase + 'statuses/user_timeline.json?screen_name=ctide&page=1&since_id=1&path=%2Fstatuses%2Fuser_timeline.json&include_rts=true&count=200' + apiSuffix,
+                           file : __dirname + '/../fixtures/synclets/twitter/home_timeline.js'});
+      return done();
+    });
+
+    it("can fetch tweets", function (done) {
+      tweets.sync(pinfo, function (err, response) {
+        if (err) return done(err);
+
+        response.data.tweet[0].id_str.should.equal('71348168469643264');
         return done();
       });
     });
