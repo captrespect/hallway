4c5bf33587d426879316009ba6baf5ef2031ee2a
Jeremie Miller <jeremie@jabber.org>
2012-03-28 23:58:31 -0500

    use the code to get an access token and provide a way to test it

--- test/authTestApp.js
+++ test/authTestApp.js
@@ -1,8 +1,10 @@
 var querystring = require('querystring');
 var express = require('express');
+var request = require('request');
 
 var hostUrl = 'http://localhost:8042';
-var client_id = 123;
+var client_id = 1;
+var client_secret = "1secret";
 var port = 8043;
 
 var app = express.createServer();
@@ -17,9 +19,21 @@ app.get('/', function(req, res) {
 })
 
 app.get('/callback', function(req, res) {
-  console.error("DEBUG: req", req);
   // would normally do the regular OAuth 2 code --> access token exchange here.
-  res.send('wahoo!');
+  var data = {
+    client_id: client_id,
+    client_secret: client_secret,
+    code: req.param('code')
+  };
+  request.post({uri:hostUrl+'/oauth/access_token', body:querystring.stringify(data), headers:{'Content-Type' : 'application/x-www-form-urlencoded'}}, function (err, resp, body) {
+    try {
+      body = JSON.parse(body);
+    } catch(err) {
+      return res.send(err, 500);
+    }
+    res.send('wahoo! <a href="'+hostUrl+'/awesome?access_token='+body.access_token+'">tokenized test</a>');
+  });
 })
 
 app.listen(port);
+
