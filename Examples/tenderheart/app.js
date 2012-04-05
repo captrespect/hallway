1d4773e6ccf9825e6569c791eb8366f0f8830079
Tyler Stalder <tyler@stalder.me>
2012-04-05 14:12:54 -0700

    add the sample auth ui app

---
+++ Examples/tenderheart/app.js
@@ -0,0 +1,45 @@
+var querystring = require('querystring');
+var express = require('express');
+var request = require('request');
+var ejs = require('ejs');
+
+var hostUrl = 'http://localhost:8042';
+var client_id = 1;
+var client_secret = "1secret";
+var port = 8043;
+
+var app = express.createServer();
+
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.static(__dirname + '/static'));
+  app.use(express.bodyParser());
+});
+
+app.get('/', function(req, res) {
+  res.render('index', {
+    layout:false
+  });
+});
+
+app.get('/callback', function(req, res) {
+  // would normally do the regular OAuth 2 code --> access token exchange here.
+  var data = {
+    client_id: client_id,
+    client_secret: client_secret,
+    code: req.param('code')
+  };
+  request.post({uri:hostUrl+'/oauth/access_token', body:querystring.stringify(data), headers:{'Content-Type' : 'application/x-www-form-urlencoded'}}, function (err, resp, body) {
+    try {
+      body = JSON.parse(body);
+    } catch(err) {
+      return res.send(err, 500);
+    }
+    res.send('wahoo! <a href="'+hostUrl+'/awesome?access_token='+body.access_token+'">tokenized test</a>');
+    //res.send('<script>window.close()</script>');
+  });
+});
+
+app.listen(port);
+
