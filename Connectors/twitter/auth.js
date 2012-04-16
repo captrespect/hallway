1eed3fa4d4eb56a12c746bdb49ea2c5080140f3f
Jeremie Miller <jeremie@jabber.org>
2012-04-16 15:43:59 -0500

    adding some more debug for remote testing

--- Connectors/twitter/auth.js
+++ Connectors/twitter/auth.js
@@ -1,5 +1,6 @@
 module.exports = {
     handler : function (host, apiKeys, done, req, res) {
+        console.error("twitter auth entry for "+host+"auth/twitter/auth and "+apiKeys.appKey);
         require('./twitter_client')(apiKeys.appKey, apiKeys.appSecret, host + "auth/twitter/auth")
         .getAccessToken(req, res, function(err, newToken) {
             if(err) return done(err);
