64c2877f71ae33765a30232381e1960f80c926f2
Jeremie Miller <jeremie@jabber.org>
2012-04-21 13:52:49 -0500

    facebook posts were updating but not tested, this also closes #66

--- test/synclet/facebook.test.js
+++ test/synclet/facebook.test.js
@@ -5,6 +5,7 @@ var mocha   = require('mocha')
   , helper  = require(path.join(__dirname, '..', 'support', 'locker-helper.js'))
   , friends = require(path.join('services', 'facebook', 'friends.js'))
   , home    = require(path.join('services', 'facebook', 'home.js'))
+  , homeup    = require(path.join('services', 'facebook', 'home_update.js'))
   , photos  = require(path.join('services', 'facebook', 'photos.js'))
   , util    = require('util')
   ;
@@ -73,6 +74,26 @@ describe("Facebook connector", function () {
         if (err) return done(err);
 
         response.data['post:42@facebook/home'][0].id.should.equal('100002438955325_224550747571079');
+        return done();
+      });
+    });
+  });
+
+  describe("home update synclet", function () {
+    beforeEach(function (done) {
+      fakeweb.registerUri({uri : apiBase + 'home?access_token=foo&date_format=U&since=yesterday&limit=100',
+                           file : __dirname + '/../fixtures/synclets/facebook/home.json'});
+      fakeweb.registerUri({uri : apiBase + 'feed?date_format=U&access_token=abc&limit=25&until=1305843879&since=yesterday',
+                           body : '{"data":[]}'});
+
+      return done();
+    });
+
+    it('can update news feed', function (done) {
+      homeup.sync(pinfo, function (err, response) {
+        if (err) return done(err);
+
+        response.data['post:42@facebook/home'][0].id.should.equal('100002438955325_105511996206765');
         return done();
       });
     });
