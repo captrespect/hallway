c06dcf154c7194bf8bf1c102bec052a1f780e2fa
Jeremie Miller <jeremie@jabber.org>
2012-04-25 14:52:07 -0500

    add a types pattern to dMap so a generic concept of a type can be configured per service and map to multiple bases

---
+++ test/lib/dmap.unit.test.js
@@ -0,0 +1,16 @@
+var should = require('should');
+var lconfig = require("lconfig");
+if (!lconfig.database) lconfig.database = {};
+lconfig.database.maxConnections = 1;
+var dMap = require('dMap');
+
+describe("dMap", function() {
+  describe("returns types", function() {
+    it("have photos", function(done){
+      var bases = dMap.types('photos',['42@facebook','42@instagram']);
+      bases.length.should.equal(2);
+      bases[0].should.equal('photo:42@facebook/photos');
+      done();
+    });
+  });
+});
