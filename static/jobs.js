0bb5d4ce1d4ec1c0c043aef234b05642f2a46f26
Beau Gunderson <beau@beaugunderson.com>
2012-06-01 22:12:42 -0700

    Sprucing up the dawg jobs page

--- static/jobs.js
+++ static/jobs.js
@@ -5,21 +5,29 @@ var states = {
   3: 'pipeline'
 };
 
-$(function() {
+function refresh() {
+  $('#rows').html('');
+
   $.getJSON('/workers/state', function(state) {
     for (var worker in state.workers) {
       state.workers[worker].active.forEach(function(job) {
-        console.log('job', job);
-
         $('#rows').append('<tr>' +
             '<td>' + worker  + '</td>' +
             '<td>' + job.synclet.connector + '#' + job.synclet.name  + '</td>' +
             '<td>' + job.profile + '</td>' +
             '<td>' + states[job.state] + '</td>' +
-            '<td>' + moment(job.tstart).fromNow() + '</td>' +
-            '<td>' + (job.tpipe ? moment(job.tpipe).fromNow() : '') + '</td>' +
+            '<td>' + moment(job.tstart).fromNow(true) + '</td>' +
+            '<td>' + (job.tpipe ? moment(job.tpipe).fromNow(true) : '') + '</td>' +
           '</tr>');
       });
     }
+  });
+}
+
+$(function() {
+  refresh();
+
+  $('#refresh').click(function() {
+    refresh();
   });
 });
