100c2cbb15e958f5c7b9127a6fe093ff96e76267
Jeremie Miller <jeremie@jabber.org>
2012-06-03 14:20:28 -0500

    grr

--- test/services/facebook.test.js
+++ test/services/facebook.test.js
@@ -31,37 +31,9 @@ describe("Facebook connector", function () {
     return done();
   });
 
-  describe("friends synclet", function () {
-    beforeEach(function (done) {
-      fakeweb.registerUri({uri : apiBase + 'friends?access_token=foo&date_format=U',
-                           file : __dirname + '/../fixtures/synclets/facebook/friends2.json'});
-      fakeweb.registerUri({uri : 'https://graph.facebook.com:443/1234?access_token=foo&date_format=U&' +
-                                 'fields=id,name,first_name,middle_name,last_name,' +
-                                 'gender,locale,languages,link,username,third_party_id,timezone,' +
-                                 'updated_time,verified,bio,birthday,education,email,hometown,' +
-                                 'interested_in,location,political,favorite_athletes,favorite_teams,' +
-                                 'quotes,relationship_status,religion,significant_other,video_upload_limits,website,work',
-                           file : __dirname + '/../fixtures/synclets/facebook/1234.json' });
-      fakeweb.registerUri({uri : 'https://graph.facebook.com:443/1234/picture?access_token=foo',
-                           file : __dirname + '/../fixtures/synclets/facebook/1234.jpg',
-                           contentType : 'image/jpeg'});
-
-      return done();
-    });
-
-    it('can fetch friend information', function (done) {
-      friends.sync(pinfo, function (err, response) {
-        if (err) return done(err);
-
-        response.data['contact:42@facebook/friends'][0].id.should.equal('1234');
-        return done();
-      });
-    });
-  });
-
   describe("home synclet", function () {
     beforeEach(function (done) {
-      fakeweb.registerUri({uri : 'https://graph.facebook.com:443/3488997579924?access_token=foo&date_format=U',
+      fakeweb.registerUri({uri : 'https://graph.facebook.com:443/?access_token=foo&date_format=U&ids=3488997579924',
                            file : __dirname + '/../fixtures/synclets/facebook/photo.json'});
       fakeweb.registerUri({uri : apiBase + 'home?access_token=foo&date_format=U&limit=200',
                            file : __dirname + '/../fixtures/synclets/facebook/home.json'});
@@ -85,7 +57,7 @@ describe("Facebook connector", function () {
 
   describe("home update synclet", function () {
     beforeEach(function (done) {
-      fakeweb.registerUri({uri : apiBase + 'home?access_token=foo&date_format=U&since=yesterday&limit=100',
+      fakeweb.registerUri({uri : apiBase + 'home?access_token=foo&date_format=U&since=yesterday&limit=500',
                            file : __dirname + '/../fixtures/synclets/facebook/home.json'});
       fakeweb.registerUri({uri : apiBase + 'feed?date_format=U&access_token=abc&limit=25&until=1305843879&since=yesterday',
                            body : '{"data":[]}'});
@@ -103,45 +75,4 @@ describe("Facebook connector", function () {
     });
   });
 
-  describe("photos synclet", function () {
-    beforeEach(function (done) {
-      fakeweb.registerUri({uri : 'https://graph.facebook.com:443/10150465363772595/photos?access_token=foo&date_format=U',
-                           file : __dirname + '/../fixtures/synclets/facebook/photos.js'});
-      fakeweb.registerUri({uri : 'https://graph.facebook.com:443/10150465363772595?access_token=foo&date_format=U',
-                           file : __dirname + '/../fixtures/synclets/facebook/album.json'});
-      fakeweb.registerUri({uri : 'https://graph.facebook.com:443/59354442594?access_token=foo&date_format=U',
-                           file : __dirname + '/../fixtures/synclets/facebook/album.json'});
-      fakeweb.registerUri({uri : 'https://graph.facebook.com:443/113387497594/photos?access_token=foo&date_format=U',
-                           file : __dirname + '/../fixtures/synclets/facebook/photos.js'});
-      fakeweb.registerUri({uri : 'https://graph.facebook.com:443/fql?q=SELECT%20object_id%2C%20modified%20FROM%20album%20WHERE%20owner%3Dme()%20AND%20modified%20%3E%200&access_token=foo',
-                           file : __dirname + '/../fixtures/synclets/facebook/albums.js'});
-      fakeweb.registerUri({uri : apiBase + 'photos?access_token=foo&date_format=U',
-                           file : __dirname + '/../fixtures/synclets/facebook/photos.js'});
-
-      return done();
-    });
-
-    it('can fetch photo albums', function (done) {
-      photos.sync(pinfo, function (err, response) {
-        if (err) return done(err);
-        response.data['album:42@facebook/albums'][0].id.should.equal('59354442594');
-        response.config.albums[0].since.should.equal(0);
-        return done();
-      });
-    });
-
-    it('can fetch photos', function (done) {
-      photos.sync(pinfo, function (err, response) {
-        if (err) return done(err);
-
-        // now that the list of albums is populated, fetch the actual list of photos
-        photos.sync(response, function (err, response) {
-          if (err) return done(err);
-
-          response.data['photo:42@facebook/photos'][0].id.should.equal('214713967594');
-          return done();
-        });
-      });
-    });
-  });
 });
