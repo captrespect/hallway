694e15c8353819d0693f482d1a6ae008fcb1142a
Beau Gunderson <beau@beaugunderson.com>
2012-06-05 16:15:28 -0700

    Improve the jobs dashboard.
    - In dawg workers/state don't use the IP as the key; make workers an
      array instead.
    - Add angry fruit salad to the jobs dashboard.
    - Add a red background to long-running jobs.
    - Display the public IP of the worker regardless of NODE_ENV.
    - When NODE_ENV=production is used, use the private IP of the worker to
      communicate (which saves money).
    - Sort the jobs list by duration.

--- static/jobs.js
+++ static/jobs.js
@@ -5,22 +5,47 @@ var states = {
   3: 'pipeline'
 };
 
+function sortTable() {
+  $('table').find('td').filter(function() {
+    return $(this).index() === 4;
+  }).sortElements(function(a, b) {
+    a = parseInt($(a).attr('data-start'), 10);
+    b = parseInt($(b).attr('data-start'), 10);
+
+    return a > b ? 1 : -1;
+  }, function() {
+    return this.parentNode;
+  });
+}
+
 function refresh() {
   $('#rows').html('');
 
   $.getJSON('/workers/state', function(state) {
-    for (var worker in state.workers) {
-      state.workers[worker].active.forEach(function(job) {
+    var i = 0;
+
+    _.sortBy(state.workers, 'publicIp').forEach(function(worker) {
+      i++;
+
+      worker.active.forEach(function(job) {
+        var classes = [];
+
+        if (job.tstart < Date.now() - (5 * 60 * 1000)) {
+          classes.push('alert');
+        }
+
         $('#rows').append('<tr>' +
-            '<td>' + worker  + '</td>' +
-            '<td>' + job.synclet.connector + '#' + job.synclet.name  + '</td>' +
+            '<td><span class="worker worker-' + i + '">' + worker.publicIp + '</span></td>' +
+            '<td>' + job.synclet.connector + '#' + job.synclet.name + '</td>' +
             '<td>' + job.profile + '</td>' +
             '<td>' + states[job.state] + '</td>' +
-            '<td>' + moment(job.tstart).fromNow(true) + '</td>' +
+            '<td data-start="' + job.tstart + '"><span class="' + classes.join(' ') + '">' + moment(job.tstart).fromNow(true) + '</span></td>' +
             '<td>' + (job.tpipe ? moment(job.tpipe).fromNow(true) : '') + '</td>' +
           '</tr>');
       });
-    }
+    });
+
+    sortTable();
   });
 }
 
