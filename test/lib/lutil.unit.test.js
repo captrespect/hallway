e5e3abd35f4c870d5b873a0490dcb58ee07a59d1
Beau Gunderson <beau@beaugunderson.com>
2012-07-10 13:59:19 -0700

    Extract isTrue from the 3 files that had it
    And add (simple) tests :)

--- test/lib/lutil.unit.test.js
+++ test/lib/lutil.unit.test.js
@@ -1,1 +1,46 @@
 var lutil = require('lutil');
+var assert = require('chai').assert;
+
+describe('lutil', function() {
+  describe('#isTrue()', function() {
+    it('should return true if true', function() {
+      assert.isTrue(lutil.isTrue(true));
+    });
+
+    it('should return true if "true"', function () {
+      assert.isTrue(lutil.isTrue("true"));
+    });
+
+    it('should return true if 1', function() {
+      assert.isTrue(lutil.isTrue(1));
+    });
+
+    it('should return true if "1"', function() {
+      assert.isTrue(lutil.isTrue("1"));
+    });
+
+    it('should return true if "yes"', function() {
+      assert.isTrue(lutil.isTrue("yes"));
+    });
+
+    it('should return false if false', function() {
+      assert.isFalse(lutil.isTrue(false));
+    });
+
+    it('should return false if 0', function() {
+      assert.isFalse(lutil.isTrue(0));
+    });
+
+    it('should return false if "0"', function() {
+      assert.isFalse(lutil.isTrue("0"));
+    });
+
+    it('should return false if "no"', function() {
+      assert.isFalse(lutil.isTrue("no"));
+    });
+
+    it('should return false if "string"', function() {
+      assert.isFalse(lutil.isTrue("string"));
+    });
+  });
+});
