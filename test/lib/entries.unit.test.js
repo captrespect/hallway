d8e6df4fdd125a057a24a54f33ab3be54ae9a61e
Jeremie Miller <jeremie@jabber.org>
2012-07-13 15:52:08 -0500

    at least hit the functions for testing to start

--- test/lib/entries.unit.test.js
+++ test/lib/entries.unit.test.js
@@ -1,1 +1,16 @@
 var entries = require('entries');
+
+describe("Entries", function() {
+  describe("parseReq", function() {
+    it("should do nothing", function(done) {
+      entries.parseReq("/", {});
+      done();
+    });
+  });
+  describe("filter", function() {
+    it("should not crash", function(done) {
+      entries.filter([], {});
+      done();
+    });
+  });
+});
